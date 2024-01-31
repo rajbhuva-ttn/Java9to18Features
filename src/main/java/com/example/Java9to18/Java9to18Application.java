191f6f0dba0e7f28885d68588e116cae84769ece
rajbhuva-ttn <bhuva.pravinbhai@tothenew.com>
2024-01-31 09:20:50 +0530

    First Commit

---
+++ src/main/java/com/example/Java9to18/Java9to18Application.java
@@ -0,0 +1,13 @@
+package com.example.Java9to18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9to18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9to18Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java9to18/Java9to18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java9to18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9to18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
